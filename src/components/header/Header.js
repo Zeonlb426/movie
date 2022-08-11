337bcdbd92cbd6f72cf2174c009d89624b6cbe7e
Dmitry <zeonlb426@gmail.com>
2022-08-11 21:41:40 +0300

    Add search and avatar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,27 @@
 import Menu from "../menu/Menu";
 import logo from '../../images/logo.svg';
+import search from '../../images/search.svg'
+import avatar from '../../images/avatar.webp'
 import './Header.css'
 
 function Header() {
   return (
     <div className="Header-block">
       <header className="Header-content">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Menu />
+        
+        <div className="block1">
+          <img src={logo} className="App-logo" />
+          <Menu />
+        </div>
+        
+        <div className="block2">
+          <img src={search} className="App-search" />
+          <div className="user">
+            <div className="App-avatar" />
+            <span>John Glich</span>
+          </div>
+        </div>
+
       </header>
     </div>
   );
